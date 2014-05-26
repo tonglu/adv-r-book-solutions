efe2928e70ae0f966c1a4698a289807beca07ce8
Tong <tong.lu@avantcredit.com>
2014-05-26 12:27:18 -0500

    Update exercise5.r

--- oo_field_guide/exercise5.r
+++ oo_field_guide/exercise5.r
@@ -9,6 +9,9 @@ g.numeric <- function(x) y
 g(10)
 # [1] 2
 
+# It starts looking for y in the environment of the method and it can't find it. Then it moves on to the env of the generic g and finds y <- 2
+
+
 h <- function(x) {
   x <- 10
   UseMethod("h")
@@ -18,5 +21,6 @@ h.numeric <- function(x) paste("num", x)
 
 h("a")
 # [1] "char a"
+# It finds x in the environment of the method h.character & h.nuemric respectively
 
 # The point is that there is a hierarchy of environments.  The specific method appropriate to the class will be searched first and then the environment of the generic function itself.  Only if neither environment contains the variable would the global environment be searched.
